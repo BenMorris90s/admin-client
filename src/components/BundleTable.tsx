d753e0d93afa2ede0e5731e734ac8827c5538f87
Ben Morris <b.morris103@gmail.com>
2022-07-21 18:10:50 +0100

    Update readme

--- src/components/BundleTable.tsx
+++ src/components/BundleTable.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function BundleTable() {
     return <div className="overflow-x-auto w-full relative">
-        <table className="w-full text-sm text-left text-slate-50 dark:text-gray-400">
-            <thead className="text-xs text-slate-50 uppercase bg-white dark:bg-gray-700 dark:text-gray-400">
+        <table className="w-full text-sm text-left text-slate-50 dark:text-gray-400 odd:bg-white even:bg-slate-50">
+            <thead className="text-xs text-slate-50 uppercase dark:bg-gray-700 dark:text-gray-400">
             <tr>
                 <th scope="col" className="py-3 px-6">
                     Product name
@@ -20,7 +20,7 @@ function BundleTable() {
             </tr>
             </thead>
             <tbody>
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <tr className="border-b dark:bg-gray-800 dark:border-gray-700">
                 <th scope="row" className="py-4 px-6 font-medium text-slate-50 whitespace-nowrap dark:text-white">
                     Apple MacBook Pro 17"
                 </th>
@@ -34,7 +34,7 @@ function BundleTable() {
                     $2999
                 </td>
             </tr>
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <tr className="border-b dark:bg-gray-800 dark:border-gray-700">
                 <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                     Microsoft Surface Pro
                 </th>
@@ -48,7 +48,7 @@ function BundleTable() {
                     $1999
                 </td>
             </tr>
-            <tr className="bg-white dark:bg-gray-800">
+            <tr className="dark:bg-gray-800">
                 <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                     Magic Mouse 2
                 </th>
