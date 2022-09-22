05a237c91591df3a8b13d9ab5401c3662cb588bb
Ben Morris <b.morris103@gmail.com>
2022-09-22 21:44:41 +0100

    Add dropzone as a component

--- src/components/CreateBundleModal.tsx
+++ src/components/CreateBundleModal.tsx
@@ -1,7 +1,8 @@
 import React from "react"
 import {useAppDispatch} from "../hooks"
 import {toggle} from "../reducers/createBundleSlice"
-import {useTranslation, Trans} from 'react-i18next'
+import {useTranslation} from 'react-i18next'
+import Dropzone from "./Dropzone";
 
 
 function CreateBundleModal() {
@@ -20,30 +21,12 @@ function CreateBundleModal() {
             <div className="fixed z-10 inset-0 overflow-y-auto">
                 <div className="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0">
 
-                    <div className="relative bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all w-2/3 h-full">
+                    <div className="relative bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all w-5/6 h-full">
                         <div className="bg-white px-4 pt-500 pb-4 sm:p-6 sm:pb-4">
                             <div className="flex flex-col items-center">
-                                <h1 className="text-lg leading-6 font-medium text-gray-900" id="modal-title">{t("Create Bundle")}</h1>
+                                <h1 className="text-lg leading-6 font-medium text-gray-900 mb-6" id="modal-title">{t("Create Bundle")}</h1>
 
-
-                                <div className="flex justify-center items-center w-full">
-                                    <label htmlFor="dropzone-file"
-                                           className="flex flex-col justify-center items-center w-full h-64 bg-gray-50 rounded-lg border-2 border-gray-300 border-dashed cursor-pointer dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-                                        <div className="flex flex-col justify-center items-center pt-5 pb-6">
-                                            <svg aria-hidden="true" className="mb-3 w-10 h-10 text-gray-400" fill="none"
-                                                 stroke="currentColor" viewBox="0 0 24 24"
-                                                 xmlns="http://www.w3.org/2000/svg">
-                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                                      d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path>
-                                            </svg>
-                                            <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><Trans><span
-                                                className="font-semibold">Click to upload</span> or drag and drop</Trans></p>
-                                            <p className="text-xs text-gray-500 dark:text-gray-400"></p>
-                                        </div>
-                                        <input id="dropzone-file" type="file" className="hidden"/>
-                                    </label>
-                                </div>
-
+                                <Dropzone/>
 
                             </div>
                         </div>
